b8e2025a5fb251ecd2009bb24e6ba384eb2d852c
Benoît Müller <82948763+Benoit-Muller@users.noreply.github.com>
2023-03-23 15:18:42 +0100

    Update BenamouBrenier.py

--- BenamouBrenier.py
+++ BenamouBrenier.py
@@ -1,5 +1,26 @@
 ''' Implementation of the Benamou_Brenier method based on fluid dynamics.
 '''
+import numpy as np
+
+class TransportProblem:
+    def __init__(self,mesh,mu,nu):
+        self.mesh = mesh
+        self.mu = mu
+        self.nu = nu
+
+    def k(rho,m):
+        tol = 1e-7
+        norm2_m = np.sum(m**2)
+        if rho >= tol:
+            return 0.5 * norm2_m / rho
+        if np.abs(rho) < tol:
+            if norm2_m < tol:
+                return 0
+            else:
+                return np.inf
+        if rho <= -tol:
+            return np.inf
+
 
 
 def poisson_step():
