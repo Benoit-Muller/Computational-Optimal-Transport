a842eadf8d8dc1322ccd15be9ad1fccfbfc2b6f3
Benoît Müller <82948763+Benoit-Muller@users.noreply.github.com>
2023-04-18 19:04:18 +0200

    Create poisson.py

---
+++ poisson.py
@@ -0,0 +1,71 @@
+print("###RUN###")
+import numpy as np
+
+import ufl
+import dolfinx
+from dolfinx import mesh, fem, io, plot
+from dolfinx.mesh import locate_entities_boundary, meshtags, locate_entities
+from ufl import ds, dx, grad, inner, Measure
+# import dolfinx_mpc
+
+from mpi4py import MPI
+from petsc4py.PETSc import ScalarType
+
+n=10 # discretization number
+msh = mesh.create_box(MPI.COMM_WORLD,[[0.0,0.0,0.0], [1.0, 1.0, 1.0]], [n, n, n], mesh.CellType.hexahedron)
+V = fem.FunctionSpace(msh, ("CG", 1)) # the space of functions
+ 
+in_neumann_boundary = lambda x: np.isclose(x[0],0) | np.isclose(x[0],1)
+out_neumann_boundary = lambda x: ~ in_neumann_boundary(x)
+boundaries = [(1, in_neumann_boundary), (2, out_neumann_boundary)]
+
+
+facet_indices, facet_markers = [], []
+fdim = msh.topology.dim - 1
+for (marker, locator) in boundaries:
+    facets = locate_entities(msh, fdim, locator)
+    facet_indices.append(facets)
+    facet_markers.append(np.full_like(facets, marker))
+facet_indices = np.hstack(facet_indices).astype(np.int32)
+facet_markers = np.hstack(facet_markers).astype(np.int32)
+sorted_facets = np.argsort(facet_indices)
+facet_tag = meshtags(msh, fdim, facet_indices[sorted_facets], facet_markers[sorted_facets])
+
+#ds = Measure("ds", domain=msh, subdomain_data=facet_tag)
+
+# Variational Problem
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+x = ufl.SpatialCoordinate(msh)
+f = 10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
+g = ufl.sin(5 * x[0]) + ufl.sin(3 * x[1]) + ufl.sin(2 * x[2])
+a = inner(grad(u), grad(v)) * dx
+L = inner(f, v) * dx + inner(g, v) * ds
+
+
+problem = fem.petsc.LinearProblem(a, L, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+
+
+# plot option 1:
+try:
+    import pyvista
+    cells, types, x = plot.create_vtk_mesh(V)
+    grid = pyvista.UnstructuredGrid(cells, types, x)
+    grid.point_data["u"] = uh.x.array.real
+    grid.set_active_scalars("u")
+    plotter = pyvista.Plotter()
+    plotter.add_mesh(grid, show_edges=True)
+    warped = grid.warp_by_scalar()
+    plotter.add_mesh(warped)
+    plotter.show(screenshot='demo_poisson.png')
+except ModuleNotFoundError:
+    print("'pyvista' is required to visualise the solution")
+    print("Install 'pyvista' with pip: 'python3 -m pip install pyvista'")
+
+
+""" unaswered questions:
+- how to create functions in function space from data
+    -> see https://fenicsproject.discourse.group/t/mapping-2d-numpy-array-into-dolfinx-function/7487/4
+
+"""
